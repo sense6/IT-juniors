9a406a67f7a9500bb6fed6a1ead042dcc5c701b2
sense6 <konrad.worobiej@gmail.com>
2017-08-16 23:48:42 +0200

    controllers update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,27 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @technologies = @user.technologies
   end
 
   def edit
+    @user = User.find(params[:id])
+    @technologies = Technology.where("id NOT IN (?)", @user.technologies.map {|t| t.id})
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "edited"
+      redirect_to users_path
+    else
+      render :edit
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name,:last_name)
+    end
 end
