c9a390b504e4d3a157e4b1f71ff67b167c7a1cd0
Wojciech Kaluzny <wojciech.kaluzny@me.com>
2017-08-16 13:17:59 +0200

    Offer decorator updated, new methods added

--- app/decorators/offer_decorator.rb
+++ app/decorators/offer_decorator.rb
@@ -5,4 +5,12 @@ class OfferDecorator < ApplicationDecorator
     "#{object.salary_min}#{offer.currency.symbol} - #{object.salary_min}#{offer.currency.symbol}"
   end
 
+  def min_salary
+    "#{object.salary_min}#{offer.currency.symbol}"
+  end
+
+  def short_desc
+    "#{object.description[0,70]}..."
+  end
+
 end
