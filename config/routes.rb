6f51b7a9c6849c03a74638909fcd8f7c0ba3b1f4
sense6 <konrad.worobiej@gmail.com>
2017-08-16 16:33:20 +0200

    routes resources users

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/show'
-
-  get 'users/edit'
 
   root to: 'offers#index'
-  resources :companies, :offers
+  resources :companies, :offers, :users
 
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
