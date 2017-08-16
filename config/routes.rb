3f4e39b61689ac1b2d3c0e068b54d18d1e8cb073
sense6 <konrad.worobiej@gmail.com>
2017-08-16 19:09:24 +0200

    routes fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'offers#index'
-  resources :companies, :offers, :users
+  resources :companies, :offers
 
   devise_for :users
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
