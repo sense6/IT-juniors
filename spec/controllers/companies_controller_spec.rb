c4fbcc768deeff9b217dfec2af8b0945bd472d31
lesin <a.kurdo@gmail.com>
2017-08-17 15:09:39 +0200

    Add basic specs for companies_controller

--- spec/controllers/companies_controller_spec.rb
+++ spec/controllers/companies_controller_spec.rb
@@ -2,26 +2,46 @@ require 'rails_helper'
 
 RSpec.describe CompaniesController, type: :controller do
   describe "GET #index" do
+    subject { get :index }
+
     it "responds successfully with an HTTP 200 status code" do
-      get :index
+      subject
       expect(response).to be_success
       expect(response).to have_http_status(200)
     end
 
     it "loads all of the companies into @companies" do
-      company1, company2 = Company.create!(name: "company name"), Company.create!(name: "test name")
-      get :index
+      company_1 = create(:company)
+      company_2 = create(:company)
 
-      expect(assigns(:companies)).to match_array([company1, company2])
+      subject
+
+
+      expect(assigns(:companies)).to match_array([company_1, company_2])
     end
   end
 
   describe "GET #show/:id" do
     it "responds successfully with an HTTP 200 status code" do
-      company = Company.create!(name: "company name")
+      company = Company.create!(name: "company name inna")
       get :show, params: { id: company.id}
       expect(response).to be_success
       expect(response).to have_http_status(200)
+    end
+  end
+
+
+  describe '#short_desc' do
+    subject { offer.decorate.short_desc(12) }
+
+    context 'description is too long' do
+      let!(:offer) { create(:offer, description: 'lorem ipsum dolor') }
+      it { is_expected.to eq("lorem ipsum...") }
+    end
+
+    context 'description is shorter than limit' do
+      let!(:offer) { create(:offer, description: 'lorem enum') }
+      it { is_expected.to eq("lorem enum") }
     end
   end
 end
