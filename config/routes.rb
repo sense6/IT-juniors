17e089d5cdd46e13aa0c333553dd4ef310995cf8
lesin <a.kurdo@gmail.com>
2017-08-17 14:13:25 +0200

    Add setting the locale from url params

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
 
   root to: 'offers#index'
-  resources :companies, :offers
-
-  get 'skills/create'
-  get 'skills/destroy'
 
   devise_for :users
   get "/users/sign_out" => "devise/sessions#destroy"
+  get '/:locale' => 'offers#index'
 
-  resources :users
+  scope "/:locale" do
+    get 'skills/create'
+    get 'skills/destroy'
+    resources :companies, :offers, :users
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
