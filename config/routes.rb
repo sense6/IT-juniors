9407694a58370a278eb765beb8c2a776e8e233bb
sense6 <konrad.worobiej@gmail.com>
2017-09-06 14:58:29 +0200

    new routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
     get 'skills_requirement/create'
     get 'skills_requirement/destroy'
     get 'skills_requirement/update'
+    get 'job_application/create'
+    get 'job_application/destroy'
+    
     resources :companies, :offers, :users
   end
 
