9acbfe7759b87007ed1df50b9defc0bb4282ce27
sense6 <konrad.worobiej@gmail.com>
2017-09-12 19:27:43 +0200

    users can now search for roles in offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -7,16 +7,18 @@ class Offer < ApplicationRecord
   has_many    :levels,        through: :skills_requirements
   has_many    :job_applications
   has_many    :applicants,    through: :job_applications, source: :user
+
   validates :title, :description, :role, :company, presence: true
 
   def self.filter_offers(search)
     if search
-      Offer.joins(:technologies).where(
-              'technologies.name LIKE ? OR
+      Offer.joins(:technologies,:role).where(
+              "technologies.name LIKE ? OR
               offers.title LIKE ? OR
               offers.description LIKE ? OR
-              offers.location LIKE ?',
-              "%#{search}%","%#{search}%","%#{search}%","%#{search}%")
+              offers.location LIKE ? OR
+              roles.name LIKE ?",
+              "%#{search}%","%#{search}%","%#{search}%","%#{search}%","%#{search}%")
     else
       all
     end
